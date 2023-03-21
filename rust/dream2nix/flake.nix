5dd6fdcf4301c40dba9ba4b8e487e63050925b7d
lize.live <git@lize.live>
2023-03-20 23:08:42 -0700

    getting error

--- rust/dream2nix/flake.nix
+++ rust/dream2nix/flake.nix
@@ -1,15 +1,31 @@
 {
-  inputs.dream2nix.url = "github:nix-community/dream2nix";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    dream2nix.url = "github:nix-community/dream2nix";
+    flake-utils.url = "github:numtide/flake-utils";
+    dream2nix.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = inp: (
-    inp.dream2nix.lib.makeFlakeOutputs {
+  outputs = inp @ {dream2nix, nixpkgs, flake-utils, ... }:
+  # let
+  #   nixpkgs = dream2nix.inputs.nixpkgs;
+  # in 
+  (
+    dream2nix.lib.makeFlakeOutputs {
       systemsFromFile = ./nix_systems;
       config.projectRoot = ./.;
       source = ./.;
       projects = ./projects.toml;
     }
+    # // flake-utils.lib.eachDefaultSystem (system: {
+    #   formatter = nixpkgs.legacyPackages.${system}.alejandra;
+    # })
     // {
-      formatter.x86_64-linux = inp.dream2nix.inputs.nixpkgs.legacyPackages.x86_64-linux.alejandra;
+      # formatter = inp.flake-utils.lib.eachDefaultSystem (system: { formatter = inp.dream2nix.inputs.nixpkgs.legacyPackages.${system}.alejandra});
+      formatter = {
+        x86_64-linux = inp.dream2nix.inputs.nixpkgs.legacyPackages.x86_64-linux.alejandra;
+        aarch64-linux = inp.dream2nix.inputs.nixpkgs.legacyPackages.aarch64-linux.alejandra;
+      };
     }
   );
 }
